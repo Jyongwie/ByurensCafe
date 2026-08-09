382fb50dc47cace70aef40fc33b551d16753e36f
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-09 10:30:18 +0700

    feat(db): create customer entity

---
+++ backend/src/main/java/byurens/entities/Customer.java
@@ -0,0 +1,48 @@
+package byurens.entities;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Customers")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer {
+    @Id
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private int loyaltyPoint;
+
+    @NotNull
+    @DecimalMin(value = "0.0")
+    @Column(nullable = false, name = "wallet_balance")
+    private BigDecimal walletBalance;
+}
