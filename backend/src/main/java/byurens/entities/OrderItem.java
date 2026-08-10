310a73c199b52d1612ff136b04aa4718505da27c
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-10 22:08:58 +0700

    feat(db): create orderitem entity

---
+++ backend/src/main/java/byurens/entities/OrderItem.java
@@ -0,0 +1,65 @@
+package byurens.entities;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Order_Items")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_variant_id")
+    private ProductVariant variant;
+
+    @NotBlank
+    @Column(nullable = false, name = "product_name")
+    private String productName;
+
+    @NotNull
+    @Column(nullable = false, name = "size_name")
+    private String sizeName;
+
+    @NotNull
+    @Column(nullable = false, name = "base_price")
+    private BigDecimal basePrice;
+
+    @Min(1)
+    @Column(nullable = false)
+    private int quantity;
+
+    private String note;
+
+    @NotNull
+    @Column(nullable = false, name = "total_price")
+    private BigDecimal totalPrice;
+}
