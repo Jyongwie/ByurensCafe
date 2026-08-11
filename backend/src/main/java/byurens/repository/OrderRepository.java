3e3fdcd872bfb990803e2fc177b16c8bf20bf878
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-11 21:15:41 +0700

    feat(repo): create OrderRepository for db access

---
+++ backend/src/main/java/byurens/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package byurens.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import byurens.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Optional<Order> findByOrderNumber(String orderNumber);
+}
