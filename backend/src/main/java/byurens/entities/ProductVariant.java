4541a855cb620155142f36fd0659e2c4dd6fd83e
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-13 22:17:46 +0700

    feat(db): add relation to recipeIngredients entity to know exactly deduct ingredient

--- backend/src/main/java/byurens/entities/ProductVariant.java
+++ backend/src/main/java/byurens/entities/ProductVariant.java
@@ -1,11 +1,15 @@
 package byurens.entities;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import byurens.enums.Size;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +19,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
@@ -68,4 +73,8 @@ public class ProductVariant {
 
     @Column(nullable = false, name = "is_promo")
     private boolean isPromo = false;
+
+    @OneToMany(mappedBy = "productVariant", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<RecipeIngredient> recipeIngredients = new ArrayList<>();
 }
