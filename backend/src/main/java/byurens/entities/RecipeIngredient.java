38dee2a71e7683299e4a4f649dc562da1bc2ddad
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-13 22:22:08 +0700

    feat(db): create RecipeIngredient entity

---
+++ backend/src/main/java/byurens/entities/RecipeIngredient.java
@@ -0,0 +1,54 @@
+package byurens.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Recipe_Ingredients")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RecipeIngredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(nullable = false, name = "product_variant_id")
+    private ProductVariant productVariant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false, name = "inventory_item")
+    private InventoryItem inventoryItem;
+
+    @NotNull
+    @DecimalMin(value = "0.0", message = "Quantity can't be negative")
+    @Column(nullable = false, name = "quantity_required")
+    private BigDecimal quantityRequired;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+}
