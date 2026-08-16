624e8409c56e256e89a08c52ec52feced0c31cdb
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-16 12:56:15 +0700

    feat(exception): add exception handling for 409 conflict and 500 server error

--- backend/src/main/java/byurens/entities/ProductVariant.java
+++ backend/src/main/java/byurens/entities/ProductVariant.java
@@ -23,7 +23,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
--- backend/src/main/java/byurens/exception/GlobalNotFoundExceptionHandler.java
+++ backend/src/main/java/byurens/exception/GlobalNotFoundExceptionHandler.java
@@ -2,15 +2,30 @@ package byurens.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import byurens.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalNotFoundExceptionHandler {
+    @ExceptionHandler(exception = OptimisticLockingFailureException.class)
+    public ResponseEntity<ErrorResponse> handleOptimisticLockingFailure(
+        OptimisticLockingFailureException ex
+    ) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.CONFLICT.value(),
+            "This record was just updated by another staff member. Please refresh the page and try again",
+            LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
     @ExceptionHandler(exception = ByurensCafeException.class)
     public ResponseEntity<ErrorResponse> handleProductNotFound(
         ByurensCafeException ex
@@ -22,5 +37,20 @@ public class GlobalNotFoundExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(exception = Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(
+        Exception ex
+    ) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "An unexpected error occured on the server",
+            LocalDateTime.now()
+        );
+
+        log.info("Critical Error: " + ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
