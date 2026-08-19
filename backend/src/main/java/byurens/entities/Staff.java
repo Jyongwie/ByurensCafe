400bdc270465a0c530d454393d6fdbe4423f6e1c
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-19 20:54:20 +0700

    refactor: using builder pattern in Staff

--- backend/src/main/java/byurens/entities/Staff.java
+++ backend/src/main/java/byurens/entities/Staff.java
@@ -19,6 +19,8 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -27,6 +29,8 @@ import lombok.Setter;
 @Table(name = "Staffs")
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Staff {
     @Id
