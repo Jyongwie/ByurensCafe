8b10d11652b80409c5f04dd2a934f28b7d5f01fb
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-19 21:14:43 +0700

    feat(service): create StaffService

---
+++ backend/src/main/java/byurens/service/StaffService.java
@@ -0,0 +1,44 @@
+package byurens.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import byurens.dto.HireStaffRequest;
+import byurens.entities.Staff;
+import byurens.entities.User;
+import byurens.exception.ByurensCafeException;
+import byurens.repository.StaffRepository;
+import byurens.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StaffService {
+    private final UserRepository userRepository;
+    private final StaffRepository staffRepository;
+
+    @Transactional
+    public Staff hireNewStaff(HireStaffRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new ByurensCafeException("Email already been registered");
+        }
+
+        User user = User.builder()
+            .email(request.email())
+            .phoneNumber(request.phoneNumber())
+            .isActive(true)
+            .passwordHash(request.rawPassword())
+            .build();
+
+        User savedUser = userRepository.save(user);
+        Staff staff = Staff.builder()
+            .user(savedUser)
+            .name(request.name())
+            .role(request.role())
+            .hourlyRate(request.hourlyRate())
+            .hireDate(request.hirDate())
+            .build();
+
+        return staffRepository.save(staff);
+    }
+}
