4f5129558a737a1a449703e13cde1d4596bd9418
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-08-28 19:57:54 +0700

    feat(dto): add dto object in RecipeIngredient and AddOn request

--- backend/src/main/java/byurens/dto/AddOnRequest.java
+++ backend/src/main/java/byurens/dto/AddOnRequest.java
@@ -1,3 +1,22 @@
 package byurens.dto;
 
-public record AddOnRequest() {}
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AddOnRequest(
+    @NotBlank
+    String name,
+    
+    @NotNull
+    @DecimalMin(value = "0.0")
+    BigDecimal price,
+
+    @NotNull
+    @DecimalMin(value = "0.0")
+    BigDecimal capital,
+
+    boolean isAvailable
+) {}
--- backend/src/main/java/byurens/dto/RecipeIngredientRequest.java
+++ backend/src/main/java/byurens/dto/RecipeIngredientRequest.java
@@ -1,3 +1,16 @@
 package byurens.dto;
 
-public record RecipeIngredientRequest() {}
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+public record RecipeIngredientRequest(
+    @NotNull
+    UUID inventoryItemId,
+
+    @NotNull
+    @DecimalMin(value = "0.0")
+    BigDecimal quantityRequired
+) {}
