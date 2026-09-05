8e307bd807abf04934a72323308225acbb8070e2
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-05 21:54:56 +0700

    feat(controller): create order controller

---
+++ backend/src/main/java/byurens/controller/OrderController.java
@@ -0,0 +1,44 @@
+package byurens.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import byurens.dto.OrderRequest;
+import byurens.dto.OrderResponse;
+import byurens.enums.OrderStatus;
+import byurens.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderRequest request) {
+        OrderResponse response = orderService.createOrder(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/status/{id}")
+    public ResponseEntity<OrderResponse> updateOrderStatus(@PathVariable UUID id, @Valid @RequestBody OrderStatus status) {
+        OrderResponse response = orderService.updateOrderStatus(id, status);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderResponse> updateOrder(@PathVariable UUID id, @Valid @RequestBody OrderRequest request) {
+        OrderResponse response = orderService.updateOrder(id, request);
+        return ResponseEntity.ok(response);
+    }
+}
