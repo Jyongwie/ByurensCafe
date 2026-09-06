a01760d9fc6ff8fb5a4df632feacdcb21558efea
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-06 09:28:54 +0700

    feat(service): create user service

---
+++ backend/src/main/java/byurens/service/UserService.java
@@ -0,0 +1,30 @@
+package byurens.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import byurens.entities.User;
+import byurens.exception.ByurensCafeException;
+import byurens.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service 
+@RequiredArgsConstructor 
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User createUserAccount(String email, String phoneNumber, String rawPassword) {
+        if (userRepository.existsByEmail(email)) {
+            throw new ByurensCafeException("Email already been registered");
+        }
+
+        User user = User.builder()
+            .email(email)
+            .phoneNumber(phoneNumber)
+            .passwordHash(passwordEncoder.encode(rawPassword))
+            .build();
+
+        return userRepository.save(user);
+    }
+}
