df36b86bdf7ad7c9437f50556f23be40935ac228
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-07 20:14:36 +0700

    feat(controller): add get method in customer controller

--- backend/src/main/java/byurens/controller/CustomerController.java
+++ backend/src/main/java/byurens/controller/CustomerController.java
@@ -1,7 +1,12 @@
 package byurens.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +28,17 @@ public class CustomerController {
     public ResponseEntity<CustomerResponse> newCustomer(@Valid @RequestBody NewCustomerRequest request) {
         CustomerResponse response = customerService.newCustomer(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping 
+    public ResponseEntity<List<CustomerResponse>> getCustomers() {
+        List<CustomerResponse> responses = customerService.getCustomers();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable UUID id) {
+        CustomerResponse response = customerService.getCustomerById(id);
+        return ResponseEntity.ok(response);
     }
 }
