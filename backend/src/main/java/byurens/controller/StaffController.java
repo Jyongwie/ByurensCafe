45a537f36a4f032db109e70aa807e170808a0bdc
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-07 19:51:04 +0700

    feat(controller): add get method in staff controller

--- backend/src/main/java/byurens/controller/StaffController.java
+++ backend/src/main/java/byurens/controller/StaffController.java
@@ -1,7 +1,12 @@
 package byurens.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +28,17 @@ public class StaffController {
     public ResponseEntity<StaffResponse> hireNewStaff(@Valid @RequestBody HireStaffRequest request) {
         StaffResponse response = staffService.hireNewStaff(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping 
+    public ResponseEntity<List<StaffResponse>> getStaffs() {
+        List<StaffResponse> responses = staffService.getStaffs();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StaffResponse> getStaffById(@PathVariable UUID id) {
+        StaffResponse response = staffService.getStaffById(id);
+        return ResponseEntity.ok(response);
     }
 }
