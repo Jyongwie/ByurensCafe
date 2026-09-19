dcff08ef41268d5af3b5aa40af1541966c8794cf
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-19 18:08:39 +0700

    feat(frontend): hydrate state on page load to prevent user to login again if they refresh the page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  private authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.restoreSession();
+  }
 }
