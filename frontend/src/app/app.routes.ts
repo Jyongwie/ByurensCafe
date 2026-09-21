1862e1bb26d9f7c902907b5daf84b4b81b68b03c
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-21 21:29:16 +0700

    refactor(frontend): refactor the route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
+import { Dashboard } from './features/pos/dashboard/dashboard';
+import { App } from './app';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
-    {path: '', redirectTo: '/login', pathMatch: 'full'}
+    {
+        path: '',
+        component: App,
+        children:[
+            {path: 'dashboard', component: Dashboard},
+            {path:'', redirectTo: 'dashboard', pathMatch: 'full'}
+        ]
+    },
+    {path: '**', redirectTo: 'login'}
 ];
