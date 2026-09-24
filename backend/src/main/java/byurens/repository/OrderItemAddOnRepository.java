56128f0e184b222690833e7ef8f9b50dea3b9494
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-25 00:14:11 +0700

    feat(repository): add custom query method to group data by hour, product name and payment method in OrderItemAddOnRepository

--- backend/src/main/java/byurens/repository/OrderItemAddOnRepository.java
+++ backend/src/main/java/byurens/repository/OrderItemAddOnRepository.java
@@ -1,14 +1,27 @@
 package byurens.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import byurens.dto.TopItemResponse;
 import byurens.entities.OrderItemAddOn;
 
 @Repository
 public interface OrderItemAddOnRepository extends JpaRepository<OrderItemAddOn, UUID> {
     Optional<OrderItemAddOn> findByAddOnName(String addOnName);
+
+    @Query(
+        "SELECT new byurens.dto.TopItemResponse(a.addOnName, SUM(oi.quantity)) " +
+        "WHERE o.createdAt >= :start AND o.createdAt <= :end AND o.paymentStatus = 'PAID' " +
+        "GROUP BY a.addOnName ORDER BY SUM(oi.quantity) DESC"
+    )
+    List<TopItemResponse> findTopAddOns(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, Pageable pageable);
 }
