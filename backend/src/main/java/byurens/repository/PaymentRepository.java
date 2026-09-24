49218a9f2bde6a6a1ae3a8c903e563a889ed1236
Jowie Ardinata <129172583+Jyongwie@users.noreply.github.com>
2026-09-25 00:20:28 +0700

    feat(repository): add custom query method to group data by hour and payment method

--- backend/src/main/java/byurens/repository/PaymentRepository.java
+++ backend/src/main/java/byurens/repository/PaymentRepository.java
@@ -1,15 +1,27 @@
 package byurens.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import byurens.dto.PaymentStatResponse;
 import byurens.entities.Payment;
 import byurens.enums.PaymentStatus;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     List<Payment> findAllByOrderIdAndStatus(UUID orderId, PaymentStatus status);
+
+    @Query(
+        "SELECT new byurens.dto.PaymentStatResponse(CAST(p.method AS string), COUNT(p.id)) " +
+        "FROM Payment p " +
+        "WHERE p.createdAt >= :start AND p.createdAt <= :end AND p.status = 'PAID' " +
+        "GROUP BY p.method"
+    )
+    List<PaymentStatResponse> findPaymentStats(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
